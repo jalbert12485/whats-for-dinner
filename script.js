7c9910cd36ad7b0f4f73e2241524edcde0716da6
Justin Albert <jalbert@carthage.edu>
2020-10-05 16:23:57 -0500

    worked on wire-framing of main page

---
+++ script.js
@@ -0,0 +1,33 @@
+$("#drinkCard").on("click",function(e){
+    e.preventDefault();
+    $("#myModal").addClass("is-active");
+
+});
+
+$("#drinkImg").on("click",function(e){
+    e.preventDefault();
+    $("#myModal").addClass("is-active");
+
+});
+
+$("#foodCard").on("click",function(e){
+    e.preventDefault();
+    $("#myModal").addClass("is-active");
+
+});
+
+$("#foodImg").on("click",function(e){
+    e.preventDefault();
+    $("#myModal").addClass("is-active");
+
+});
+
+$("#modalx").on("click",function(e){
+    e.preventDefault();
+    $('#myModal').removeClass("is-active");
+})
+
+$("#modalCancel").on("click",function(e){
+    e.preventDefault();
+    $('#myModal').removeClass("is-active");
+})
