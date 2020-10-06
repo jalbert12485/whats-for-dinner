722c3effce6c62f068f3621ee8fa484c8014a5c0
Matthew Hiatt <mhiatt62@gmail.com>
2020-10-06 13:44:48 -0500

    script test

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
-function turnOnModal(e){
-    e.preventDefault();
-    $("#myModal").addClass("is-active");
+function turnOnModal(e) {
+  e.preventDefault();
+  $("#myModal").addClass("is-active");
 }
 
-function turnOffModal(e){
-    e.preventDefault();
-    $("#myModal").removeClass("is-active");
+function turnOffModal(e) {
+  e.preventDefault();
+  $("#myModal").removeClass("is-active");
 }
 
 
-$(document).on("click",".modal-on",turnOnModal);
-$(document).on("click",".modal-off",turnOffModal);
+$(document).on("click", ".modal-on", turnOnModal);
+$(document).on("click", ".modal-off", turnOffModal);
 
-function searchFoodRecipe(){
-    var search="chicken";
-    var queryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s="+search;
+function searchFoodRecipe() {
+  var search = "chicken";
+  var queryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + search;
 
-    $.ajax({
+  $.ajax({
       url: queryURL,
       method: "GET"
     })
-      .then(function(response) {
-          console.log(response);
-      });
+    .then(function (response) {
+      console.log(response);
+    });
 }
 
+//We will remove this later
 
+function randomMeal() {
+  var queryURL = "https://www.themealdb.com/api/json/v1/1/random.php";
 
-function randomMeal(){
-    var queryURL = "https://www.themealdb.com/api/json/v1/1/random.php";
-
-    $.ajax({
+  $.ajax({
       url: queryURL,
       method: "GET"
     })
-      .then(function(response) {
-        console.log(response.meals[0].strMealThumb);
-          $("#food-img").attr("src",response.meals[0].strMealThumb); 
-      });
- 
+    .then(function (response) {
+      console.log(response.meals[0].strMealThumb);
+      $("#food-img").attr("src", response.meals[0].strMealThumb);
+    });
 
-     
+
+
 }
 
 randomMeal();
