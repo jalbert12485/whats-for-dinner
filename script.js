02ba9937449ee1b83759c6049a005ca42aa110b7
Justin Albert <jalbert@carthage.edu>
2020-10-07 14:06:40 -0500

    added functionality for drink favorite/view/remove buttons

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ if(favoriteFoods===null){
   localStorage.setItem("favoriteFoods",JSON.stringify(favoriteFoods));
 }
 
+// Variable to enable saving of favorite foods
+var favoriteDrinks=JSON.parse(localStorage.getItem("favoriteDrinks"));
+if(favoriteDrinks===null){
+  favoriteDrinks=[];
+  localStorage.setItem("favoriteDrinks",JSON.stringify(favoriteDrinks));
+}
+
 
 
 // Function to turn on modal which will display recipes
@@ -57,6 +64,7 @@ function randomDrink(){
         $("#drink-img").attr("src",response.drinks[0].strDrinkThumb); 
         $("#drink-thumb").attr("src",response.drinks[0].strDrinkThumb); 
         $("#drink-name").text(response.drinks[0].strDrink);
+        $("#drink-name").attr("data-number",response.drinks[0].idDrink);
       });    
 }
 
@@ -64,13 +72,12 @@ randomDrink();
 
 // Allows the user to save a displayed item to their favorites.
 $("#save-food").on("click",saveFavoriteFood);
-
+$("#save-drink").on("click",saveFavoriteDrink);
 
 // Gets the id of the displayed food, find the entry in the api and saves all the info into the favorites for later use.
-function saveFavoriteFood(option) {
+function saveFavoriteFood() {
   var favoriteID=Number($("#food-title").data("number"));
   var favoriteName=$("#food-title").text();
-  var queryURL = "https://www.themealdb.com/api/json/v1/1/lookup.php?i="+favoriteID;
   var isSaved=false;
   for(var i=0; i<favoriteFoods.length; i++){
     if(favoriteFoods[i][0]==favoriteID){
@@ -84,8 +91,28 @@ function saveFavoriteFood(option) {
   }
 }
 
-$("#view-fav-food").on("click", viewFavoriteFood);
+function saveFavoriteDrink() {
+  var favoriteID=Number($("#drink-name").data("number"));
+  var favoriteName=$("#drink-name").text();
+  var isSaved=false;
+  for(var i=0; i<favoriteDrinks.length; i++){
+    if(favoriteDrinks[i][0]==favoriteID){
+      isSaved=true;
+    }
+  }
 
+  if(!(isSaved)){
+  favoriteDrinks.push([favoriteID,favoriteName]);
+  localStorage.setItem("favoriteDrinks",JSON.stringify(favoriteDrinks));
+  }
+}
+
+
+
+$("#view-fav-food").on("click", viewFavoriteFood);
+$("#view-fav-drink").on("click", viewFavoriteDrink);
+
+// $("#view-fav-drink").on("click", viewFavoriteDrink);
 //Opens the modal and displayed your saved favorite recipes.
 function viewFavoriteFood(){
   $("#modal-title").text("Favorite Food Recipes");
@@ -96,13 +123,37 @@ function viewFavoriteFood(){
   for(var i=0; i< favoriteFoods.length; i++){
   var newLi=$("<li>");
   newLi.addClass("columns is-vcentered")
-  var newText=$("<j3>");
+  var newText=$("<h3>");
   newText.text(favoriteFoods[i][1]);
   newText.addClass("column is-four-fifths is-tablet");
   var removeButton=$("<button>");
   removeButton.text("Remove");
   removeButton.addClass("column is-one-fifth is-tablet");
-  removeButton.attr("data-remove",i);
+  removeButton.attr("data-removefood",i);
+  newLi.append(newText);
+  newLi.append(removeButton);
+  newUl.append(newLi);
+  }
+
+  $("#modal-body").append(newUl);
+}
+
+function viewFavoriteDrink(){
+  $("#modal-title").text("Favorite Drink Recipes");
+  $("#modal-body").empty();
+
+  var newUl=$("<ul>");
+
+  for(var i=0; i< favoriteDrinks.length; i++){
+  var newLi=$("<li>");
+  newLi.addClass("columns is-vcentered")
+  var newText=$("<h3>");
+  newText.text(favoriteDrinks[i][1]);
+  newText.addClass("column is-four-fifths is-tablet");
+  var removeButton=$("<button>");
+  removeButton.text("Remove");
+  removeButton.addClass("column is-one-fifth is-tablet");
+  removeButton.attr("data-removedrink",i);
   newLi.append(newText);
   newLi.append(removeButton);
   newUl.append(newLi);
@@ -113,12 +164,18 @@ function viewFavoriteFood(){
 
 //In the modal containing favorite recipes, allows the remove button to remove items from your favorites.
 $("#my-modal").on("click",function (event){
-  var removeInd=event.target.dataset.remove;
-  if(removeInd != null)
-  {
-    favoriteFoods.splice(removeInd,1);
+  var removeIndFood=event.target.dataset.removefood;
+  var removeIndDrink=event.target.dataset.removedrink;
+  console.log(removeIndDrink);
+  if(removeIndFood != null){
+    favoriteFoods.splice(removeIndFood,1);
     localStorage.setItem("favoriteFoods",JSON.stringify(favoriteFoods));
     viewFavoriteFood();
+  }
+  if(removeIndDrink != null){
+    favoriteDrinks.splice(removeIndDrink,1);
+    localStorage.setItem("favoriteDrinks",JSON.stringify(favoriteDrinks));
+    viewFavoriteDrink();
   }
 
 
