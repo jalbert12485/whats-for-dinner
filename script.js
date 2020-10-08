73e26069756ffb3f542cb79c3b4b30b6da8b1ee6
Matthew Hiatt <mhiatt62@gmail.com>
2020-10-08 11:08:29 -0500

    new up

--- script.js
+++ script.js
@@ -23,9 +23,8 @@ function searchFoodRecipe() {
     .then(function (response) {
       console.log(response);
     });
-}
 
-//We will remove this later
+}
 
 function randomMeal() {
   var queryURL = "https://www.themealdb.com/api/json/v1/1/random.php";
@@ -39,8 +38,47 @@ function randomMeal() {
       $("#food-img").attr("src", response.meals[0].strMealThumb);
     });
 
-
-
 }
 
 randomMeal();
+
+function findByDrink() {
+  var alch = "drinkName";
+  var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + search;
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+    accept: "application/JSON",
+  }).then(function (response) {
+    console.log(response);
+  })
+}
+
+function randomDrink() {
+  var drink = "randomDrinkName";
+  var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+  $.ajax({
+      url: queryURL,
+      method: "GET",
+      accept: "application/JSON",
+    }).then(function () {
+
+        .then(function (response) {
+          console.log(response);
+          if (window.localStorage) {
+            var search = document.getElementById("searchBox");
+            search.addEventListener("change", function () {
+              localStorage.setItem("searchBox", search.value);
+            }, false);
+          }
+        });
+        if (window.localStorage) {
+          var search = document.getElementById("searchBox");
+          searchBox.value = localStorage.getItem("searchBox");
+          search.value = localStorage.setItem("searchBox");
+          searchBox.value = sessionStorage.setItem("searchBox");
+        }
+        // add for() for over-all forcast (data to go into)
+        for (var search = i; i > 0; i++);
+        var col = $("<div>").addClass("row-md-3");
+        var card = $("<div>").addClass("card bg-primary text-white");
